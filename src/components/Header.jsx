75b01b2094eb654cf418619abe71c019e66add9f
Munyemanzi <Herve.Munyemanzi@gmail.com>
2025-01-27 09:38:05 +0100

    accueil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
       </div>
 
       <div className="flex-1 flex flex-col justify-center items-center">
-        <h1 className="font-bold text-3xl p-2">React Movie</h1>
+        <h1 className="font-bold text-3xl p-2">Movie App</h1>
         <div className="flex-1 flex flex-col py-2 rounded-xl">
           <input className="bg-white" type="text" />
-          <button className="bg-blue-600">Rechercher</button>
+          <button className="bg-blue-600 p-2">Rechercher</button>
         </div>
       </div>
     </div>
